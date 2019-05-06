9077d12ce91197e4fef898b8e4d5b79fd6c643c6
Shane <shanegates@hotmail.com>
2019-05-06 15:54:40 -0700

    simplify the css reset for player & computer choices

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,12 +18,20 @@ function newRound() {
 $(document).ready(function () {
     const validChoices = rockPaperScissors.getAvailableChoices();
 
+    const rock = $("#rock");
+    const paper = $("#paper");
+    const scissors = $("#scissors");
+
+    const myRock = $("#myRock");
+    const myPaper = $("#myPaper");
+    const myScissors = $("#myScissors");
+
     newGame();
 
     $(document).keyup(function (event) {
         let keyPressed = event.key.toLowerCase();
 
-        $(".choices").css("display", "none");
+        $(".choices").css({ display: "none", left: ""});
 
         if (rockPaperScissors.getGameStarted()) {
             if (event.which === 32)// spacebar
@@ -36,48 +44,40 @@ $(document).ready(function () {
 
                 let result = rockPaperScissors.compareChoices(keyPressed);
 
-                $("#rock").css({ left: "" });
-                $("#paper").css({ left: "" });
-                $("#scissors").css({ left: "" });
-
-                $("#myRock").css({ left: "" });
-                $("#myPaper").css({ left: "" });
-                $("#myScissors").css({ left: "" });
-
                 switch (result.computerChoice) {
                     case 'r':
-                        $("#rock").css("display", "block");
+                        rock.css("display", "block");
                         break;
                     case 'p':
-                        $("#paper").css("display", "block");
+                        paper.css("display", "block");
                         break;
                     case 's':
-                        $("#scissors").css("display", "block");
+                        scissors.css("display", "block");
                         break;
                 }
 
                 switch (result.playerChoice) {
                     case 'r':
-                        $("#myRock").css("display", "block");
+                        myRock.css("display", "block");
                         break;
                     case 'p':
-                        $("#myPaper").css("display", "block");
+                        myPaper.css("display", "block");
                         break;
                     case 's':
-                        $("#myScissors").css("display", "block");
+                        myScissors.css("display", "block");
                         break;
                 }
 
                 if (result.hasComputerWon) {
                     switch (result.computerChoice) {
                         case 'r':
-                            $("#rock").animate({ left: '-250px' });
+                            rock.animate({ left: '-250px' });
                             break;
                         case 'p':
-                            $("#paper").animate({ left: '-250px' });
+                            paper.animate({ left: '-250px' });
                             break;
                         case 's':
-                            $("#scissors").animate({ left: '-250px' });
+                            scissors.animate({ left: '-250px' });
                             break;
                     }
                 }
@@ -85,13 +85,13 @@ $(document).ready(function () {
                 if (result.hasPlayerWon) {
                     switch (result.playerChoice) {
                         case 'r':
-                            $("#myRock").animate({ left: '250px' });
+                            myRock.animate({ left: '250px' });
                             break;
                         case 'p':
-                            $("#myPaper").animate({ left: '250px' });
+                            myPaper.animate({ left: '250px' });
                             break;
                         case 's':
-                            $("#myScissors").animate({ left: '250px' });
+                            myScissors.animate({ left: '250px' });
                             break;
                     }
                 }
